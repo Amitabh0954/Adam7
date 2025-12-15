c32835af56c7c012050db7ee066960d1f37ad342
Amitabh0954 <141054453+Amitabh0954@users.noreply.github.com>
2025-12-15 11:51:51 +0530

    Create or update file app/controllers/example_controller.py

---
+++ app/controllers/example_controller.py
@@ -0,0 +1,10 @@
+from flask import Blueprint, jsonify
+
+example_bp = Blueprint('example', __name__)
+
+@example_bp.route('/example', methods:['GET'])
+def get_example():
+    response = {
+        'message': 'This is an example endpoint'
+    }
+    return jsonify(response)
