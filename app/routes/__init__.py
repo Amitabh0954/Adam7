18b9e3030c8cb19fab6956dd489ca2340ac82847
Amitabh0954 <141054453+Amitabh0954@users.noreply.github.com>
2025-12-15 11:52:31 +0530

    Add routes/__init__.py

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,10 +1,5 @@
 from flask import Blueprint
-
-main_bp = Blueprint('main', __name__)
-
-@main_bp.route('/')
-def home():
-    return 'Hello, World!'
+from app.controllers.example_controller import example_bp
 
 def register_routes(app):
-    app.register_blueprint(main_bp)
+    app.register_blueprint(example_bp)
