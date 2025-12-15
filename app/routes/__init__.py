320067126480bb16d754bb38a09f0aebb2e30afb
Amitabh0954 <141054453+Amitabh0954@users.noreply.github.com>
2025-12-15 11:51:35 +0530

    Create __init__.py file in routes directory

---
+++ app/routes/__init__.py
@@ -0,0 +1,10 @@
+from flask import Blueprint
+
+main_bp = Blueprint('main', __name__)
+
+@main_bp.route('/')
+def home():
+ return 'Hello, World!'
+
+def register_routes(app):
+ app.register_blueprint(main_bp)
