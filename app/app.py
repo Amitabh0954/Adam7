857bbd355c465e5c579f45cbb5844d106e038545
Amitabh0954 <141054453+Amitabh0954@users.noreply.github.com>
2025-12-15 11:51:34 +0530

    Create or update file: app/app.py

---
+++ app/app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from app.routes import register_routes
+
+app = Flask(__name__)
+
+app.config.from_pyfile('config/config.py')
+
+register_routes(app)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
